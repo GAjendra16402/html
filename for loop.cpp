ae5a63dd22712f35e8eca0e9aec6ffe2446e342c
GAjendra16402 <gajendrasingh16402@gmail.com>
2023-07-29 21:19:38 +0530

    today work

---
+++ for loop.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,a;
+	printf("enter the number in a ");
+	scanf("%d",&a);
+	for(i=a;i<=100;i++)
+	{
+		printf("%d\n",a++);
+	}
+	return 0;
+}
+
+// program to print the counting from desired number of user.
